ffc026f9772d504ad7ded29e0dbb5649971d914e
easydong19 <ymh1353@naver.com>
2024-08-27 12:58:02 +0900

    리액트 네이티브를 다루는 기술 4장

--- TodoApp/components/AddTodo.js
+++ TodoApp/components/AddTodo.js
@@ -9,10 +9,11 @@ import {
   Platform,
   Keyboard,
 } from 'react-native';
-function AddTodo() {
+function AddTodo({onInsert}) {
   const [text, setText] = useState('');
 
   const onPress = () => {
+    onInsert(text);
     setText('');
     Keyboard.dismiss();
   };
